63525236dba76c4db0433b7a2250e4b111852ce9
Matheus Machado Fiuza <matheusmachadofiuza@gmail.com>
2025-06-03 02:33:41 +0000

    Making stuff closer to a working state (maybe)

--- README.md
+++ README.md
@@ -4,16 +4,20 @@ Secret project!
 
 # How to build
 ```
+rm -rf build
 cmake -B build -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Release
 cmake --build build --parallel ${nproc}
 cmake --install build
 ```
 
-git clone https://github.com/JuliaInterop/libcxxwrap-julia.git
-cd libcxxwrap-julia
+# How to build libcxxwrap-julia
+```
+cd third-party/libcxxwrap-julia
+rm -rf build
+rm -rf install
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=../../third-party/jlcxx ..
+cmake -DCMAKE_INSTALL_PREFIX=../install ..
 cmake --build . --config Release -j $(nproc)
 cmake --install .
-cd ../../
-rm -rf libcxxwrap-julia
+cd ../../../
+```
--- bindings/julia/build_tarballs.jl
+++ bindings/julia/build_tarballs.jl
@@ -16,33 +16,42 @@ sources = [
 
 script = raw"""
 
-cd $WORKSPACE/srcdir
+    cd $WORKSPACE/srcdir
 
-cmake -B build \
-    -DCMAKE_INSTALL_PREFIX=${prefix} \
-    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DCMAKE_BUILD_TYPE=Release
+    # Build libcxxwrap-julia
+    cd third-party/libcxxwrap-julia
+    rm -rf build
+    rm -rf install
+    mkdir build && cd build
+    cmake -DCMAKE_INSTALL_PREFIX=../install ..
+    cmake --build . --config Release -j $(nproc)
+    cmake --install .
+    cd ../../../
 
-cmake --build build --parallel ${nproc}
-
-cmake --install build
+    # Build libnumav_jl
+    cmake -B build \
+        -DCMAKE_INSTALL_PREFIX=${prefix} \
+        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+        -DCMAKE_BUILD_TYPE=Release
+    cmake --build build --parallel ${nproc}
+    cmake --install build
 
 """
 
-platforms = Platform("x86_64", "linux", libc=:musl)
+platforms = Platform("x86_64", "linux", libc=:glibc)
 platforms = expand_cxxstring_abis(platforms)
 
 products = [
     LibraryProduct("libnumav", :libnumav),
-    FileProduct("share/numav/Numav.jl", :Numav_jl)
 ]
 
 dependencies = [
-    Dependency("libcxxwrap_julia_jll")
+    Dependency("libcxxwrap_julia_jll"),
+    BuildDependency("Julia_jll")
 ]
 
 build_tarballs(
-    ARGS, name, version, sources, script, platforms, products, dependencies;
-    julia_compat="1.7",
-    preferred_gcc_version=v"9"
+    ARGS, name, version, sources, script, platforms, products, dependencies#;
+    # julia_compat="1.7",
+    # preferred_gcc_version=v"9"
 )
