537267111b2eba435fd4f743639c89a805bb94f9
root <matheus.fiuza@numav.com.br>
2025-10-25 18:30:30 +0000

    update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -12,7 +12,7 @@ int main()
         NumericalMethod::FEM,
         Domain::FREQUENCY,
         Dimension::D3,
-        ElementOrder::O2
+        ElementOrder::O1
     >();
 
     // load the mesh
@@ -31,32 +31,33 @@ int main()
     s.add_volume_material(
         2,
         [](auto f) { return 1.2; },
-        [](auto f) { return std::complex<double>(343,0); }
+        [](auto f) { return std::complex<double>(343,5); }
     );
 
     // add volume velocity sources
-    auto q =[](auto f) { return 1/f; };
+    auto q = [](auto f) { return 1/f; };
     s.add_sound_source(
         TypeOfSource::POINT, {3.0, 2.0, 1.0},
         PhysicalQuantity::VOLUME_VELOCITY, q
     );
-    auto u =[](auto f) { return 1/f; };
+    auto u = [](auto f) { return 1/f; };
     s.add_sound_source(
         TypeOfSource::SURFACE, 2,
         PhysicalQuantity::PARTICLE_VELOCITY, u
     );
 
-    // // add pressure sources
-    // auto p =[](auto f) { return 1/f; };
-    // s.add_sound_source(
-    //     TypeOfSource::POINT, {2.0, 2.5, 1.0},
-    //     PhysicalQuantity::PRESSURE, p
-    // );
-    // s.add_sound_source(
-    //     TypeOfSource::SURFACE, 3,
-    //     PhysicalQuantity::PRESSURE, p
-    // );
-
+    // add pressure sources
+    auto p0 = [](auto f) { return 1; };
+    s.add_sound_source(
+        TypeOfSource::POINT, {4.0, 1.0, 0.5},
+        PhysicalQuantity::PRESSURE, p0
+    );
+    auto p1 = [](auto f) { return 2; };
+    s.add_sound_source(
+        TypeOfSource::SURFACE, 3,
+        PhysicalQuantity::PRESSURE, p1
+    );
+ 
     // add specific surface acoustic impedance
     auto Z = [](auto f) { return std::complex<double>(4000, 4000); };
     s.add_surface_specific_acoustic_impedance(5, Z);
